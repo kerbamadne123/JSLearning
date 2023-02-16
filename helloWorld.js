fda8bd05b4c25382b2394a554f0808d6a6092120
kerbamadne123 <kerbamadne123@gmail.com>
2023-02-16 13:41:12 +0530

    create Variable and Variables Declaration and Initialization
    create Variable and Variables Declaration and Initialization.

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,13 @@
 console.log("kerba madne");
+console.log("29");
+console.log("pune");
+var fullName="kerba Madne";
+console.log(fullName);
+console.log(fullName);
+var city;  //variable Declaration
+console.log(city);
+city="Pune";  //variable initialization
+console.log(city);
+city="Nanded";
+console.log(city);
+
